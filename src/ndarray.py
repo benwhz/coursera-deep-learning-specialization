4961a4320ad929221b9742af9b2da3447db5370a
Ben Wang <benwhz@foxmail.com>
2024-03-05 15:37:39 +0800

    update

--- src/ndarray.py
+++ src/ndarray.py
@@ -1,9 +1,8 @@
 import numpy as np
 
-print(np.__version__)
-arr = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]], [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]])
-
-print(arr, arr.shape, type(arr))
+#print(np.__version__)
+#arr = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]], [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]])
+#print(arr, arr.shape, type(arr))
 
 def create_test():
     arr = np.array(['5','6','7'],ndmin=0)
@@ -23,5 +22,16 @@ def create_test():
 
     print(np.eye(6, k=4, dtype=int))
 
+from transformers import DistilBertTokenizerFast
+from transformers import DistilBertForQuestionAnswering
+
+text = ['The garden is north of the bathroom. The hallway is south of the bathroom.', 'What is north of the bathroom? These']
+def tokenizer_test():
+    tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-cased')
+    encoding = tokenizer(text[1], text[0], truncation=True, padding=True, max_length=tokenizer.model_max_length)
+    print(encoding.input_ids)
+
+    pytorch_model = DistilBertForQuestionAnswering.from_pretrained("model/pytorch")
+
 if __name__ == '__main__':
-    create_test()
+    tokenizer_test()
